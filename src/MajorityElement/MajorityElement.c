81d2b20f4027a0c3c30c695b4166dbfb7af07ab7
Pioupia <pioupiadeux@gmail.com>
2024-03-24 22:20:40 +0100

    Feat (MajorityElement): Start solving the Majority element challenge

---
+++ src/MajorityElement/MajorityElement.c
@@ -0,0 +1,28 @@
+int majorityElement(int* nums, int numsSize) {
+    int i, k = 0;
+    int min = (numsSize / 2) + 1;
+    int default_number = 0;
+
+    if (numsSize)
+        default_number = nums[0];
+
+    while (i <= min)
+    {
+        if (nums[i] == default_number && i)
+        {
+            i++;
+            continue;
+        }
+        k = 1;
+        for (int j = i + 1; j < numsSize; j++)
+        {
+            if (nums[j] == nums[i])
+                k++;
+            nums[j] = default_number;
+        }
+        if (k >= min)
+            return (nums[i]);
+        i++;
+    }
+    return (0);
+}
